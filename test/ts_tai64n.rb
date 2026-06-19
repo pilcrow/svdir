939d50d6ecf81f7c99f6a121a9acd32c35e70bfc
pilcrow <mjp@pilcrow.madison.wi.us>
2026-06-19 13:35:22 -0500

    Refactor TAI64N tests to derive lo32 from StatusBytes::TAI_EPOCH

--- test/ts_tai64n.rb
+++ test/ts_tai64n.rb
@@ -21,38 +21,50 @@ class TC_Tai64N < Test::Unit::TestCase
     [pid].pack('V') + [pause].pack('c') + [want].pack('a')
   end
 
-  # The epoch conversion from TAI64 to Unix is:
-  #   unix = tai64 - 2^62 - 10
-  # where +10 is the fixed TAI-UTC offset at the epoch.
-  #
-  # For a desired Unix timestamp U, the stored lo32 = U + 10.
-  # hi32 is always 0x40000000 (= 2^30) for any post-epoch time.
+  # The on-wire TAI64 value is:  tai64 = unix_epoch + TAI_EPOCH
+  # where TAI_EPOCH = 2^62 + 10 (the TAI representation of Unix time 0).
+  # StatusBytes reverses this:  unix_epoch = tai64 - TAI_EPOCH.
+  def tai64_from_unix(unix_epoch)
+    unix_epoch + StatusBytes::TAI_EPOCH
+  end
 
-  # Unix epoch 2009-02-13 23:31:20 +0000 = 1234567880.
-  # lo32 = 1234567880 + 10 = 1234567890 = 0x499602D2
+  # split a 64-bit value into hi/lo 32-bit halves for big-endian packing
+  def hi32(val)
+    val >> 32
+  end
+
+  def lo32(val)
+    val & 0xFFFFFFFF
+  end
+
+  # Unix epoch 2009-02-13 23:31:20 +0000 = 1234567880
   def test_known_timestamp
-    buf = make_status(0x40000000, 0x499602D2, 500_000_000)
+    tai64 = tai64_from_unix(1234567880)
+    buf = make_status(hi32(tai64), lo32(tai64), 500_000_000)
     sb  = StatusBytes.new(buf)
     assert_in_delta(1234567880.5, sb.epoch, 0.000_001)
   end
 
-  # Zero nanoseconds at Unix epoch 1000
+  # Zero nanoseconds
   def test_zero_nano
-    buf = make_status(0x40000000, 1010, 0) # lo32 = 1000 + 10 = 1010
+    tai64 = tai64_from_unix(1000)
+    buf = make_status(hi32(tai64), lo32(tai64), 0)
     sb  = StatusBytes.new(buf)
     assert_in_delta(1000.0, sb.epoch, 0.000_001)
   end
 
-  # Max nanoseconds at Unix epoch 1000
+  # Max nanoseconds (999999999)
   def test_max_nano
-    buf = make_status(0x40000000, 1010, 999_999_999)
+    tai64 = tai64_from_unix(1000)
+    buf = make_status(hi32(tai64), lo32(tai64), 999_999_999)
     sb  = StatusBytes.new(buf)
     assert_in_delta(1000.999999999, sb.epoch, 0.000_001)
   end
 
-  # Verify that elapsed returns a positive Float (time-dependent)
+  # elapsed returns a positive Float (epoch 0 edge case)
   def test_elapsed
-    buf = make_status(0x40000000, 10, 500_000_000)
+    tai64 = tai64_from_unix(0)
+    buf = make_status(hi32(tai64), lo32(tai64), 500_000_000)
     sb  = StatusBytes.new(buf)
     assert_kind_of(Float, sb.elapsed)
     assert(sb.elapsed > 0)
